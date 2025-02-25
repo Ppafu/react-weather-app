bf71fe59a2bd06e85718b347d3bfb84648314a8f
Inessa <inessa.evs@gmail.com>
2025-02-25 15:07:48 +0300

    minor improvements

--- src/hooks/useSearch.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import { useGeocoding } from "./useGeocoding";
-
-export function useSearch() {
-  const [cityList, setCityList] = useState([]);
-  const { geocoding, geocodingError, isPendingGeocoding } =
-    useGeocoding(cityName);
-}
